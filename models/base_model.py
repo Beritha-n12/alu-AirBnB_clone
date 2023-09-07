bbd1408b914b859eacf087323af47beb342c5b68
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-09-07 20:05:34 +0200

    Updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -3,20 +3,30 @@
 This module defines a base class for
 all models in our hbnb clone
 """
-import models
+
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel():
     """
     BaseModel that defines all common
     attributes/methods for other classe
     """
- 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        def __init__(self, *args, **kwargs):
+        if kwargs:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
