eab6cef7929dad6fca34a9534e55ae990f59e2fc
Niyotwagira Beritha <b.niyotwagi@alustudent.com>
2023-09-07 14:30:11 +0000

    models

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,32 @@
 #!/usr/bin/python3
-""" Module for Base """
-
+"""
+This module defines a base class for
+all models in our hbnb clone
+"""
+import uuid
 from datetime import datetime
-from uuid import uuid4
-import json
 import models
 
-format_date = "%Y-%m-%dT%H:%M:%S.%f"
-
-
 class BaseModel:
-    """ Basemodel class """
-    def __init__(self, *args, **kwargs):
-        """ Initialization of Database """
-        if args is not None and len(args) > 0:
-            pass
-        if kwargs:
-            for key, item in kwargs.items():
-                if key in ['created_at', 'updated_at']:
-                    item = datetime.strptime(item, format_date)
-                if key not in ['__class__']:
-                    setattr(self, key, item)
-        else:
-            self.id = str(uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            models.storage.new(self)
+ """
+    BaseModel that defines all common
+    attributes/methods for other classe
+    """
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self):
-        """str definition"""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """save definition"""
         self.updated_at = datetime.now()
-        models.storage.new(self)
-        models.storage.save()
 
     def to_dict(self):
-        new_dict = self.__dict__.copy()
-        new_dict['__class__'] = self.__class__.__name__
-        new_dict['created_at'] = self.created_at.isoformat()
-        new_dict['updated_at'] = self.updated_at.isoformat()
-        return new_dict
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+
