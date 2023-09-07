38da7fd66f410ccfe81024ac8234b9b3df5d494e
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-09-07 19:35:30 +0200

    small changes

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,7 @@ import uuid
 from datetime import datetime
 
 class BaseModel():
- """
+    """
     BaseModel that defines all common
     attributes/methods for other classe
     """
@@ -16,20 +16,17 @@ class BaseModel():
     def __init__(self):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        class_name = self.__class__.__name__
-        obj_dict = self.__dict__.copy()
+        obj_dict = dict(self.__dict__)
+        obj_dict['created_at'] = self.__dict__['created_at'].isoformat()
+        obj_dict['updated_at'] = self.__dict__['updated_at'].isoformat()
         obj_dict['__class__'] = self.__class__.__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
-     
         return obj_dict
