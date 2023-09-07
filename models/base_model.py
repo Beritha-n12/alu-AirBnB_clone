f0ce645b2518e836fd677680ee4d44e138b8b229
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-09-07 18:23:29 +0200

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,12 +18,14 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self):
+        class_name = self.__class__.__name__
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        class_name = self.__class__.__name__
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
